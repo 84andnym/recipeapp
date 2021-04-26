1e3d7754f4d0e15455458b0a1a5f8f7d525b29d2
Andreas Nyman <70688270+84andnym@users.noreply.github.com>
2021-04-26 13:44:18 +0200

    Added dropdown and routings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { MyFirstComponent } from './components/myfirstcomponent/MyFirstComponent'
 import { Routing } from './routes/Routing'
 import { NavigationBar } from './components/navigationbar/NavigationBar'
+import {UserProvider} from './shared/global/provider/UserProvider'
+import  './shared/global/css/Global.css'
 function App() {
   return (
+    <UserProvider>
     <Routing>
-      <h1>React Tutorial!</h1>
-      < MyFirstComponent name="Andreas" age=" 37" />
+      <NavigationBar />
     </Routing>
+    </UserProvider>
 
   );
 }
--- src/view/HomeView.js
+++ src/view/HomeView.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const HomeView = () => {
+  const [count, setcount] = useState(0)
+
+  useEffect(() => {
+    ("Component is being rendered")
+    return () => {
+      ("Component is being removed")
+    }
+  }, [count])
+
   return (
     <div>
-      <h1>This is the Home view</h1>
+      <h1>{count}</h1>
+      <button onClick={() => setcount(count + 1)}>Increment with 1</button>
     </div>
   )
 }
